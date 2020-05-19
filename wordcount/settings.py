f5d642934a5f05d1577bc37b6d795fc8baa07f36
shivam <shivshopextra011@gmai.com>
2020-05-19 13:20:23 +0530

    SecondDeploy

--- wordcount/settings.py
+++ wordcount/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'n0wv5g#ytn03_1fh3fm2tc$_s$8v%j50(eq8cxhcmey(la_gh+'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['djangotest.eba-a3tjksbj.us-west-2.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['django-env.eba-c7y8uhwk.us-west-2.elasticbeanstalk.com']
 
 
 # Application definition
