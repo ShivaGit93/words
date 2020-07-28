7d7eb28c0eaebc1a4cb1412ba500f90bd63dfd33
shivam <shivshopextra011@gmai.com>
2020-07-28 21:10:05 +0530

    Final2

--- wordcount/urls.py
+++ wordcount/urls.py
@@ -14,10 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path
+from django.contrib import admin
 from . import views
 urlpatterns = [
     path('', views.home, name= ''),
     path('count/', views.count, name='count'),
     path('about/',views.about, name='about'),
+    path('admin/', admin.site.urls),
+
 
 ]
